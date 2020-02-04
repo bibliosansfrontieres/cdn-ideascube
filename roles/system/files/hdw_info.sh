3c3d87b0bbbf55fc66513a152ab753a6bd192249
Florian <florian.heslouin@bibliosansfrontieres.org>
2020-02-04 17:10:33 +0100

    Add flexibility on the storage device

--- roles/system/files/hdw_info.sh
+++ roles/system/files/hdw_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE="/media/hdd/log/hdw_info.txt"
+FILE="/tmp/hdw_info.txt"
 echo "##################################" >> $FILE
 echo "#          lscpu                 #" >> $FILE
 echo "##################################" >> $FILE
